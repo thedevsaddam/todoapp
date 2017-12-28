87e40ba438f55af8411ef6c19a1436d26087bc6e
Saddam H <thedevsaddam@gmail.com>
2017-12-28 12:12:28 +0600

    Asset moved

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func init() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	err := rnd.Template(w, http.StatusOK, []string{"home.tpl"}, nil)
+	err := rnd.Template(w, http.StatusOK, []string{"static/home.tpl"}, nil)
 	checkErr(err)
 }
 
